b12a70b1267c910db7c77132759ab99823ab787a
peter88213 <petert88213@gmail.com>
2025-07-24 21:02:47 +0200

    Removed docstring template

--- src/test.py
+++
@@ -1,7 +0,0 @@
-"""
-
-Copyright (c) 2025 Peter Triesberger
-For further information see https://github.com/peter88213/cnv_genlog
-Published under the MIT License 
-(https://opensource.org/licenses/mit-license.php)
-"""
